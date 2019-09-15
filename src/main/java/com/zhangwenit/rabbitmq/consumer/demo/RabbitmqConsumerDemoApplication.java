7fa939a27200a54d16f97edb20391ee60468c46c
zhangwen <zhangwenit@126.com>
2019-09-15 16:04:52 +0800

    first commit

---
+++ src/main/java/com/zhangwenit/rabbitmq/consumer/demo/RabbitmqConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhangwenit.rabbitmq.consumer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqConsumerDemoApplication.class, args);
+    }
+
+}
